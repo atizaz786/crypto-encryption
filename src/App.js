7afbd60730d33b984649e6a53497b718ff6769a9
atizaz786 <your_email@example.com>
2023-05-15 15:40:56 +0100

    feature: changing layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
 import { Tabs } from 'antd';
-
 import Encrypter from './pages/encrypter';
 import Decrypter from './pages/decrypter';
 
-const { TabPane } = Tabs;
 
-const onChange = (key) => {
-  console.log(key);
-};
 const items = [
   {
     key: '1',
@@ -28,13 +23,6 @@ function App() {
   return (
     <div className="App" style={{ padding: '2rem', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
     <Tabs defaultActiveKey="1" items={items} centered />
-      {/* <TabPane tab="Encrypt" key="1">
-        <Encrypter />
-      </TabPane>
-      <TabPane tab="Decrypt" key="2">
-        <Decrypter />
-      </TabPane>
-    </Tabs> */}
   </div>
   );
 }
